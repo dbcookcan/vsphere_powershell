bef0da81f810355b67ab9a18b2df58bf28f4a0f5
David Cook <dbc_github@advan.ca>
2020-12-01 16:16:27 -0500

    First Commit

---
+++ Delete-Snapshots.ps1
@@ -0,0 +1,71 @@
+#!/bin/pwsh
+@"
+===============================================================================
+Title       : Delete-Snapshots.ps1
+Description : Consolidate all snapshots on the VMware platform.
+
+Usage       : .\Delete-Snapshots.ps1 or Powershell IDE
+Date        : 01/12/2020
+AUTHOR      : David Cook
+REQUIRES    : VMware PowerCLI
+===============================================================================
+"@
+
+#
+# Set local vars
+$VER=1.01
+$ReportName = "Delete-Snapshots"
+$ScriptName = $MyInvocation.MyCommand.Definition
+IF ($EMAIL -gt 1){ $EMAIL = 1 }
+$emailfrom="davidc@advan.ca"
+$emailto="davidc@advan.ca"
+
+
+#
+# Include Kira common header
+. "./Kira_Include.ps1"
+
+
+#
+# Connect to Virtual Center
+$VC = Connect-VIServer -Server $VIServer -Port $port -user $VIusername `
+      -password $VIpassword -ErrorAction SilentlyContinue
+
+If (!$?) {
+
+  #
+  # Error - failed to connect to the virtual center server
+  Write-Host "ERROR: Failed to connect to Virtual Center: $VIServer"
+
+} Else {
+
+
+  #
+  # Successful login to Virtualcenter .. yay!
+
+
+  #
+  # Retrieve sorted list of all VMs on the platform
+  $VMLIST=get-vm | Sort-Object
+  #| where-object { $_.GuestId -like "centos*" }
+
+  #
+  # Loop through list
+  ForEach( $VM in $VMLIST ){
+    Write-Output $VM.Name
+    $snaps=Get-Snapshot -vm $VM.Name
+
+    # If snapshots exist, consolidate them including children.
+    If ($snaps){
+        Write-Host "Consolidating snapshots for "$VM.Name
+        $snaps | Remove-Snapshot -RemoveChildren -RunAsync -confirm:$false
+    } # end if snaps
+  } # end foreach vm
+
+  #
+  # Disconnect from Virtualcenter
+  Disconnect-Viserver -Confirm:$false
+
+} # end if connect
+
+# END OF SCRIPT
---
+++ Kira_Include.ps1
@@ -0,0 +1,109 @@
+@"
+===============================================================================
+Title       : Kira_Header.ps1
+Description : Standard Header for Kira Systems PowerShell
+            :
+            : Called as a "sourced" file at the top to provide features
+            : from a common library.
+
+Usage       : To be included in other scripts with:
+                        . "./Kira_Include.ps1"
+Date        : 21/03/2019
+AUTHOR      : David Cook
+REQUIRES    : VMware PowerCLI
+===============================================================================
+History
+Ver             Date            Who     Details
+v1.00   21/03/2019      dbc     Initial delivery
+v1.01   17/04/2019      dbc     Added in-app declarations if not defined
+                                elsewhere.
+				Add credential store support.
+v1.02	01/12/2020	dbc	Modified for PowerShell Core 7.
+===============================================================================
+"@
+
+#
+# Set common vars across Kira instances
+$port="443"
+$OutputBaseDir = "/opt/automate/Reports"
+$filedate = Get-Date -format yyyy.M.d
+$CredDir = "/opt/automate/CredStore/"
+$KeyfileName = "MasterKey.txt"
+$KeyFile = $CredDir+$KeyfileName
+$tab=[char]9
+
+# Set typical vars used within applications if they have not previously been
+# declared elsewhere
+If (!$DEBUG) { $DEBUG=0 }
+If (!$ERRTOT) { $ERRTOT=0 }
+
+
+#
+# Retrieve the Master Key file from common store. This is a 192-bit encryption
+# key that was generated with the gen-keyfile.ps1 program. It forms the salt
+# for all encryption of credential stores.
+$key=Get-Content -Path $KeyFile
+if( $DEBUG -gt 0 ){ Write-Host "Key: $key" }
+
+#
+# If no command line parms were provided we will interactively get the
+# credential
+# information the user.
+if( [string]::IsNullOrEmpty($VIServer) ){
+    if( $DEBUG -gt 0 ){ 
+      Write-Host "No VIServer on command line, interactive prompting"
+    }
+    $VIServer = read-host "$ReportName`n`nVirtualCenter or ESXI Host Name"
+} # END If Server string empty
+if( [string]::IsNullOrEmpty($VIUsername) ){
+    if( $DEBUG -gt 0 ){
+       Write-Host "No VIUser on command line, interactive prompting"
+    }
+    $VIusername = read-host "$ReportName`n`nUsername for "$VIServer
+} # END: If Username string is empty
+
+
+#
+# Generate Credential Store filename
+$CredFile=$CredDir+$VIServer+"-"+$VIusername+".cred"
+if( $DEBUG -gt 0 ){ Write-Host "Credential File: $CredFile" }
+
+
+#
+# If credential file exists, we read it in and associate them with the
+#  user/password
+# variables.
+If (Test-Path $CredFile) {
+   # Credential File exists
+   # Read them back
+
+   #
+   # Retrieve encrypted credential and store as PSCredential object
+   $MyCreds = New-Object -TypeName System.Management.Automation.PSCredential `
+                 -ArgumentList $VIUsername, (Get-Content $CredFile | `
+                 ConvertTo-SecureString -Key $key)
+
+   #
+   # Assign PSCredential object back to local vars because login parameters
+   # are different depending on how they were obtained. Local vars becomes
+   # lowest-common-denominator
+   $VIsecurepassword=$MyCreds.Password
+   $VIpassword = (New-Object PSCredential "$VIusername",$VIsecurepassword).GetNetworkCredential().Password
+
+   # Clear credential store
+   $MyCreds=""
+
+} else {
+
+   #
+   # No credential file, have to get info from user
+   if( $DEBUG -gt 0 ){ Write-Host "Credential File does not exist. Prompt user" }
+
+   # Get info from user interactively
+   $VIsecurepassword = read-host -assecurestring "$ReportName`n`nPassword"
+   $VIpassword = (New-Object PSCredential "$VIusername",$VIsecurepassword).GetNetworkCredential().Password
+
+} # END: Test Credential file exist
+
+
+# END OF SCRIPT
